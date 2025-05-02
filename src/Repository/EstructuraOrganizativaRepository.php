06e5cb8fb07068f210cbbf6b997a37fd02e3b62d
mbaezgregoric <baezgregoric@gmail.com>
2025-05-02 08:41:02 -0400

    feat - Nuevo endpoint de estructura organizativa

--- src/Repository/EstructuraOrganizativaRepository.php
+++ src/Repository/EstructuraOrganizativaRepository.php
@@ -8,6 +8,8 @@ use Doctrine\Persistence\ManagerRegistry;
 
 use Symfony\Component\Security\Core\Security;
 
+use App\Dto\EstructuraOrganizativaOutPutDto;
+
 /**
  * @method EstructuraOrganizativa|null find($id, $lockMode = null, $lockVersion = null)
  * @method EstructuraOrganizativa|null findOneBy(array $criteria, array $orderBy = null)
@@ -23,6 +25,68 @@ class EstructuraOrganizativaRepository extends ServiceEntityRepository
         parent::__construct($registry, EstructuraOrganizativa::class);
     }
 
+
+    /**
+     * Listar Estructura Organizativa.
+     */
+    public function findList()
+    {   
+        $entityManagerDefault = $this->getEntityManager();
+        $dataEstructuraOrganizativa=[];
+        $query = $entityManagerDefault->createQueryBuilder();
+        $allAppointmentsQuery = $query->select('estructuraorganizativa.id,estructuraorganizativa.jerarquia,estructuraorganizativa.estructura_organizativa')
+        ->from(EstructuraOrganizativa::class,'estructuraorganizativa')
+        ->where('estructuraorganizativa.padre_id is null')
+        ->addOrderBy('estructuraorganizativa.id', 'ASC')
+        ->getQuery();
+        $queryult = $query->getQuery();
+        $data =  $queryult->execute();
+        $cont=0;
+        foreach($data as $clave=>$valor){
+          $profesionDto =new EstructuraOrganizativaOutPutDto();
+          $profesionDto->id=$valor["id"];
+          //$profesionDto->padre_id=$valor->getPadreId();
+          $profesionDto->estructuraorganizativa=$valor["estructura_organizativa"];
+          $profesionDto->jerarquia=$valor["jerarquia"];
+
+          //$profesionDto->estructuraorganizativa=$valor["estructura_organizativa"];
+          //$profesionDto->iddireccionalmacen=($valor->getIddireccionalmacen()!=null)?array("id"=>$valor->getIddireccionalmacen()->getId(),"Nombre"=>$valor->getIddireccionalmacen()->getNombre(),"Direccion"=>$valor->getIddireccionalmacen()->getDireccionzona()):[]; 
+
+          $dataEstructuraOrganizativa[]=$profesionDto;
+      }
+       return array("data"=>$dataEstructuraOrganizativa);
+    }
+
+
+     /**
+     * Listar Estructura Organizativa id.
+     */
+    public function findListid($id)
+    {
+        $entityManagerDefault = $this->getEntityManager();
+        $dataEstructuraOrganizativa=[];
+        $query = $entityManagerDefault->createQueryBuilder();
+        $allAppointmentsQuery = $query->select('estructuraorganizativa.id,estructuraorganizativa.jerarquia,estructuraorganizativa.estructura_organizativa')
+        ->from(EstructuraOrganizativa::class,'estructuraorganizativa')
+        ->where("estructuraorganizativa.padre_id ='".$id."'")
+        ->addOrderBy('estructuraorganizativa.id', 'ASC')
+        ->getQuery();
+        $queryult = $query->getQuery();
+        $data =  $queryult->execute();
+        $cont=0;
+        foreach($data as $clave=>$valor){
+          $profesionDto =new EstructuraOrganizativaOutPutDto();
+          $profesionDto->id=$valor["id"];
+          //$profesionDto->padreid=$id;
+          //$profesionDto->nivelunidad=$valor["nivel_unidad"];
+          $profesionDto->estructuraorganizativa=$valor["estructura_organizativa"];
+          $profesionDto->jerarquia=$valor["jerarquia"];
+          //$profesionDto->iddireccionalmacen=($valor->getIddireccionalmacen()!=null)?array("id"=>$valor->getIddireccionalmacen()->getId(),"Nombre"=>$valor->getIddireccionalmacen()->getNombre(),"Direccion"=>$valor->getIddireccionalmacen()->getDireccionzona()):[]; 
+
+          $dataEstructuraOrganizativa[]=$profesionDto;
+      }
+       return array("data"=>$dataEstructuraOrganizativa);
+    }
         
     // /**
     //  * @return EstructuraOrganizativa[] Returns an array of EstructuraOrganizativa objects
