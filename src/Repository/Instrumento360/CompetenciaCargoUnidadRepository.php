736886e0a31f45f7c3dda8472d37cc246b6d00c2
mbaezgregoric <baezgregoric@gmail.com>
2025-06-13 20:32:42 -0400

    Fix: Actualizar findTipoByInput estructura recursiva

--- src/Repository/Instrumento360/CompetenciaCargoUnidadRepository.php
+++ src/Repository/Instrumento360/CompetenciaCargoUnidadRepository.php
@@ -172,24 +172,59 @@ class CompetenciaCargoUnidadRepository extends ServiceEntityRepository
 
     public function Estructura_Organizativa($idestructura){
     $dataEstructura=[];    
+    $regSalida=false;
     $entityManager = $this->getEntityManager();
     $queryresp = $entityManager->createQueryBuilder();
     $estructuraQuery = $queryresp->select('eo.id, eo.padre_id, eo.estructura_organizativa')
         ->from(EstructuraOrganizativa::class, 'eo')
         ->where('eo.id = :id')
-        ->orWhere('eo.padre_id = :id') // Filtra también por padre_id
         ->setParameter('id', $idestructura)
         ->getQuery();
         $queryrespdata = $queryresp->getQuery();
         $dataresp =  $queryrespdata->execute();
-        foreach($dataresp as $clave=>$valorresp){
-        $dataArea[] = array(
-            "id" => $valorresp["id"],
-            "label" => $valorresp["estructura_organizativa"]
+        $regUnidad =  $dataresp[0]["padre_id"];
+
+        $dataEstructura[] = array(
+            "id" => $dataresp[0]["id"],
+            "label" => $dataresp[0]["estructura_organizativa"]
         );
+
+  do {
+        $queryRecursivo = $entityManager->createQueryBuilder();
+        $estructuraQueryRec = $queryRecursivo->select('eo.id, eo.padre_id, eo.estructura_organizativa')
+        ->from(EstructuraOrganizativa::class, 'eo')
+        ->where('eo.id = :id')
+        ->setParameter('id', $regUnidad)
+        ->getQuery();
+        $queryrecursivodata = $queryRecursivo->getQuery();
+        $datarecursivo =  $queryrecursivodata->execute();
+
+        if (is_null($datarecursivo[0]["padre_id"])) {
+            $dataEstructura[] = array(
+                "id" => $datarecursivo[0]["id"],
+                "label" => $datarecursivo[0]["estructura_organizativa"]
+            );
+            $regSalida=true;
+        }else{
+            $regUnidad =  $datarecursivo[0]["padre_id"];
+            $dataEstructura[] = array(
+                "id" => $datarecursivo[0]["id"],
+                "label" => $datarecursivo[0]["estructura_organizativa"]
+            );
         }
-    return $dataArea;
-  }
+        
+
+   } while (!$regSalida);
+   // Ordenar el array de menor a mayor por el campo 'id'
+    usort($dataEstructura, function($a, $b) {
+        return $a['id'] <=> $b['id'];
+    });
+   return $dataEstructura;
+
+}
+
+  
+
 
 
 
